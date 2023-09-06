9bce885245a9131b368a0671ebbb2407b38f86b9
Linkerin <130695447+Linkerin@users.noreply.github.com>
2023-09-06 14:26:20 +0300

    ADD: unit tests for badge's truncateContent function

--- lib/components/BadgeInline/truncateContent.ts
+++ lib/components/BadgeInline/truncateContent.ts
@@ -1,20 +1,25 @@
 /**
  * Truncates badge content to the given number of characters
- * @param content Badge content
- * @param length Maximum number of content characters
+ * @param content The badge content
+ * @param length The maximum number of content characters
  * @returns For `string` content returns a truncated string of specified length.
  * For `number` content returns a maximum number of previous order
  * and `+` symbol at the end. Thus, the return value length is `length` + 1.
+ *
+ * Negative numbers will be rounded and then treated like strings.
  *
  * @example
  * truncateBadgeContent('John Dorian', 4) // 'John'
  * truncateBadgeContent(13756, 4) // '9999+'
  */
 function truncateContent(content: string | number, length: number) {
-  if (typeof length !== 'number') return content;
-  if (typeof content !== 'number' && typeof content !== 'string') {
-    return content;
+  if (typeof length !== 'number') {
+    throw new Error("Content's max length value has to be a number");
   }
+  if (typeof content !== 'number' && typeof content !== 'string') {
+    throw new Error('Content has to be a string or a number');
+  }
+
   // For strings just return first `length` characters
   if (typeof content === 'string') {
     return content.slice(0, length);
@@ -22,6 +27,10 @@ function truncateContent(content: string | number, length: number) {
 
   const fixedNum = content.toFixed(0);
   if (fixedNum.length <= length) return fixedNum;
+
+  if (content < 0) {
+    return fixedNum.slice(0, length);
+  }
 
   /**
    * Maximum number of previous order
