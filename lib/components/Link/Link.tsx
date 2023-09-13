a9905ba1c66d115185f8218bbd6776ab81c928ac
Linkerin <130695447+Linkerin@users.noreply.github.com>
2023-09-13 20:26:07 +0300

    FIX: Link

--- lib/components/Link/Link.tsx
+++ lib/components/Link/Link.tsx
@@ -1,26 +1,80 @@
-export interface LinkProps<T> extends React.ComponentPropsWithoutRef<'a'> {
-  /**
-   * HTML element that will be rendered by the component
-   */
-  as?: T;
-  /**
-   * Link's font color
-   */
-  color?: 'primary' | 'inherit';
-  /**
-   * Do not display an icon for an external link
-   */
-  externalIcon?: boolean;
-  /**
-   * Sets whether the link leads to an external resource
-   */
-  isExternal?: boolean;
-  /**
-   * Use this link for a link overlay
-   */
-  overlay?: boolean;
-  /**
-   * Undeline options
-   */
-  underline?: 'none' | 'hover' | 'always';
-}
+'use client';
+
+import { createElement, forwardRef, lazy, Suspense } from 'react';
+import classNames from 'classnames';
+
+import { LinkProps } from './Link.types';
+import useKeyboardFocusOutline from '../../hooks/useKeyboardFocusOutline';
+
+const ExternalIcon = lazy(
+  () => import('../PuiIcons/ExternalIcon/ExternalIcon')
+);
+
+import styles from './Link.module.scss';
+
+export type { LinkProps };
+
+/**
+ * Link component that wraps an HTML `<a>` element.
+ * Could be used with different routing libraries link components via `as` prop.
+ * @see {@link https://www.w3.org/WAI/ARIA/apg/patterns/link | WAI-ARIA | Link Pattern}
+ * @see {@link https://particles.snipshot.dev/docs/components/link | Particles UI | Link}
+ */
+const Link = forwardRef<
+  HTMLAnchorElement,
+  LinkProps<'a' | React.ComponentType<any>>
+>(function Link(
+  {
+    children,
+    className,
+    onBlur,
+    onKeyUp,
+    rel,
+    underline,
+    as = 'a',
+    color = 'primary',
+    externalIcon = true,
+    isExternal = false,
+    overlay = false,
+    target = '_self',
+    ...props
+  },
+  ref
+) {
+  const { onBlurHandler, onKeyUpHandler, outlineDefaultClassName } =
+    useKeyboardFocusOutline({
+      onBlur,
+      onKeyUp
+    });
+
+  return createElement(
+    as,
+    {
+      ref,
+      className: classNames(
+        styles.link,
+        outlineDefaultClassName,
+        { [styles[color]]: !!color && color !== 'inherit' },
+        { [styles.external]: isExternal },
+        {
+          [styles[`underline-${underline}`]]: underline && underline !== 'none'
+        },
+        className
+      ),
+      target: isExternal ? '_blank' : target,
+      rel: isExternal && !rel ? 'noopener external' : rel,
+      'data-pui-component': overlay ? 'overlay-link' : 'link',
+      onBlur: onBlurHandler,
+      onKeyUp: onKeyUpHandler,
+      ...props
+    },
+    children,
+    isExternal && externalIcon && (
+      <Suspense>
+        <ExternalIcon />
+      </Suspense>
+    )
+  );
+});
+
+export default Link;
