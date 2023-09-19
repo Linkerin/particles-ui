bfdbcaa4df1ee45ff330e8fc7f20ebea8e6ee4de
Linkerin <130695447+Linkerin@users.noreply.github.com>
2023-09-19 13:45:56 +0300

    ADD: storybook storysource (code) addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
     'storybook-dark-mode',
+    '@storybook/addon-storysource',
     {
       name: '@storybook/addon-styling',
       options: {
