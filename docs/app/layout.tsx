3e5ec26516e88b8ef9f601fc0d8959bfeec2c8d1
Linkerin <130695447+Linkerin@users.noreply.github.com>
2023-10-12 16:43:19 +0300

    fix: 🐛 Removed unnecessary lib imports

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
 
-import { PuiTheme, ThemeProvider } from '../../src';
-import Spectrum from '../../src/theme/spectrum/spectrum';
-
-import '../../src/styles/pui-base.scss';
 import styles from './layout.module.css';
 
 const manrope = Manrope({ subsets: ['latin'] });
@@ -14,9 +10,6 @@ export const metadata: Metadata = {
   description: 'React UI library based compatible with Next.js 13.'
 };
 
-const secondary = new Spectrum('hsl', '175, 28%, 54%');
-const tertiary = new Spectrum('hsl', '29, 66%, 55%');
-
 export default function RootLayout({
   children
 }: {
@@ -25,12 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en" data-pui-mode="system">
       <body className={manrope.className}>
-        <PuiTheme mainColors={{ light: { secondary, tertiary } }} />
-        <ThemeProvider>
-          <div className={styles.container}>
-            <main>{children}</main>
-          </div>
-        </ThemeProvider>
+        <div className={styles.container}>
+          <main>{children}</main>
+        </div>
       </body>
     </html>
   );
